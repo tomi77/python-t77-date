34dbb7dcf3d956e173f6b7f076c9d7a4b7481d5b
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2015-12-22 23:35:59 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,29 @@
-from setuptools import setup
+import codecs
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(filepath):
+    filepath = os.path.join(os.path.dirname(__file__), filepath)
+    return codecs.open(filepath, 'r', 'utf-8').read()
+
+description = read('README.md')
 
 setup(
     name="t77-date",
+    version='0.1',
+    description='',
+    long_description=description,
     author="Tomasz Jakub Rup",
     author_email="tomasz.rup@gmail.com",
+    license='MIT',
+    url='https://github.com/tomi77/python-t77-date',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+    ],
+    zip_safe=False,
     test_suite="tests",
 )
