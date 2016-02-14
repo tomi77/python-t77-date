d0507754a12484bb99c5750e8492e9e7754dbbfa
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2016-02-14 19:20:55 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
-from setuptools import setup
+from codecs import open
+
+from setuptools import setup, find_packages
 
 
 setup(
@@ -8,19 +10,23 @@ setup(
     author_email="tomasz.rup@gmail.com",
     url='https://github.com/tomi77/python-t77-date',
     description='A set of functions related with dates',
-    long_description='',
+    long_description=open("README.md").read(),
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Utilities',
     ],
     license='MIT',
-    packages=['t77_date']
+    packages=find_packages(exclude=('tests',))
 )
